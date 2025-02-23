e0d61e66d5d8ba532de21f18f2055a6211d30dde
John Aughey <jha@aughey.com>
2025-02-23 14:06:31 +0000

    works

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,40 @@
-use std::{pin, time::Duration};
-
 #[allow(dead_code)]
 async fn async_foo(f: impl AsyncFn() -> i32) -> i32 {
     f().await
 }
 
-// Debounces an async button press
-// The async fn is given the button state that will not return
-// until it is NOT that state.  It will return the new state of the button.
-// (assert: the new state of the button != the old state of the button)
-async fn debounce<T>(
-    wait_until_not: impl AsyncFn(T) -> T,
-    current_value: T,
-    steady_duration: std::time::Duration,
-    sleep: impl AsyncFn(Duration) -> (),
-) where
-    T: PartialEq,
+/// Debounces an async "button" press.  This doesn't have to be an actual button, just something that
+/// logically has an unpressed and a not-unpressed state.  The transition of unpressed and not unpressed
+/// can be noisy, so this function will wait until the state is stable before returning the new state.
+///
+/// Stability is defined as the stable_wait future completing while the state is in the not-unpressed state.
+
+/// wait_until_not: a function that takes the current state of the button and returns the new state once it no longer current state.
+/// current_value: the current state of the button which is passed to the wait_until_not function.
+/// sleep: Provides an async wait that, if it expires, means that the value is stable.
+pub async fn debounce<T, WAIT, SLEEP>(
+    wait_until_not: WAIT,
+    not_pressed_value: T,
+    stable_wait: SLEEP,
+) -> T
+where
+    T: PartialEq + Clone,
+    WAIT: AsyncFn(T) -> T,
+    SLEEP: AsyncFn() -> (),
 {
     loop {
-        let new_value = wait_until_not(current_value).await;
+        let new_value = wait_until_not(not_pressed_value.clone()).await;
 
-        let s = sleep(steady_duration);
-        let w = wait_until_not(new_value);
+        let s = stable_wait();
+        let w = wait_until_not(new_value.clone());
         futures::pin_mut!(s, w);
-        futures::select! {
-            new_value = w => {
+        match futures::future::select(s, w).await {
+            futures::future::Either::Left((_, _)) => {
+                // The sleep finished first, so we return the new value
                 return new_value;
             }
-            () = s => {
-                // loop again
+            futures::future::Either::Right((_, _)) => {
+                // The wait finished first, so we continue the loop
             }
         }
     }
@@ -48,5 +54,29 @@ mod tests {
         let f = || async { 42 };
         let result = async_foo(f).await;
         assert_eq!(result, 42);
+    }
+
+    #[tokio::test]
+    async fn test_debounce() {
+        let count = std::cell::Cell::new(0);
+        let wait_until_not = |_old_state: i32| {
+            async {
+                if count.get() <= 10 {
+                    count.replace(count.get() + 1);
+                    count.get()
+                } else {
+                    // Wait forever
+                    loop {
+                        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+                    }
+                }
+            }
+        };
+        let stable_time = || async move {
+            tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+        };
+
+        let result = debounce(wait_until_not, 0, stable_time).await;
+        assert_eq!(result, 11);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,23 @@ use anyhow::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    println!("Hello, world!");
+    let wait_until_not = move |old_state: i32| {
+        async move {
+            if old_state <= 10 {
+                old_state + 1
+            } else {
+                // Wait forever
+                loop {
+                    tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+                }
+            }
+        }
+    };
+    let sleep = || async move {
+        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+    };
+
+    let result = asyncplay::debounce(wait_until_not, 0, sleep).await;
+    assert_eq!(result, 11);
     Ok(())
 }
